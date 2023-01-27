e2600cd921ee0ef43f61074f1b8d9bc897c0472e
Asifa <asifashaik333@gmail.com>
2023-01-27 19:39:46 +0530

    cc

--- Lwc-practice-project/force-app/main/default/lwc/gitListViewComponent/gitListViewComponent.js
+++ Lwc-practice-project/force-app/main/default/lwc/gitListViewComponent/gitListViewComponent.js
@@ -6,11 +6,14 @@ import {
     MessageContext
 } from 'lightning/messageService';
 import searchMessage from '@salesforce/messageChannel/SampleMessageChannel__c';
+import insertContact from '@salesforce/apex/gitComponentContriller.insertContact';
 const QUERY_USER_ENDPOINT_URL='https://api.github.com/search/users?q=';
 
 export default class GitListViewComponent extends LightningElement {
 
     @api personName;
+    selecteduser ='';
+    selecteduserArray=[];
     retrivedusers=[];
     subscription = null;
 
@@ -55,4 +58,34 @@ export default class GitListViewComponent extends LightningElement {
         unsubscribe(this.subscription);
         this.subscription = null;
     }
+
+    handleonuserclicked(event){
+       
+        console.log(event.detail); 
+        
+    this.selecteduser=event.detail;
+    this.selecteduserArray.push(event.detail);
+      //alert(this.selecteduser);
+    }
+    get showuser(){
+        return this.selecteduser.length != 0 ? true: false;
+    }
+
+//    async handleSaveUserClick(){
+// try{
+// const issuccess=await insertContact({contactName:this.selecteduser});
+// console.log('created creation'+issuccess);
+// }catch(error){
+// console.log(error);
+// }
+//     }
+
+    async handleSaveUserClick(){
+        try{
+        const issuccess=await insertContact1({contactNameList:this.selecteduserArray});
+        console.log('created creation'+issuccess);
+        }catch(error){
+        console.log(error);
+        }
+            }
 }
--- Lwc-practice-project/force-app/main/default/lwc/gitlistviewChild/gitlistviewChild.js
+++ Lwc-practice-project/force-app/main/default/lwc/gitlistviewChild/gitlistviewChild.js
@@ -2,4 +2,10 @@ import { api, LightningElement } from 'lwc';
 
 export default class GitlistviewChild extends LightningElement {
     @api retrivedUser;
+    handlebuttonClick(){
+        const selectedEvent = new CustomEvent('userclicked', { detail: this.retrivedUser.login });
+
+        // Dispatches the event.
+        this.dispatchEvent(selectedEvent);
+    }
 }
